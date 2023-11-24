269bf6701ad701b5c3d772fe12d39589dea0f71f
Isabela V. Santos <isabela.vieir@hotmail.com>
2023-11-24 15:51:38 -0300

    csv reader

--- src/main/java/dis/ultrassom/Ultrassom.java
+++ src/main/java/dis/ultrassom/Ultrassom.java
@@ -6,6 +6,17 @@ package dis.ultrassom;
 
 import org.jblas.DoubleMatrix;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Arrays;
+
 /**
  *
  * @author Isabela V. Santos
@@ -13,82 +24,70 @@ import org.jblas.DoubleMatrix;
 public class Ultrassom {
 
     public static void main(String[] args) {
-        //System.out.println("Hello World!");
-        // Definindo matrizes M e N e vetor a
-        /*DoubleMatrix M = new DoubleMatrix(new double[][]{{92,99,1,8,15,67,74,51,58,40},
-                                                         {98,80,7,14,16,73,55,57,64,41},
-                                                         {4,81,88,20,22,54,56,63,70,47},
-                                                         {85,87,19,21,3,60,62,69,71,28},
-                                                         {86,93,25,2,9,61,68,75,52,34},
-                                                         {17,24,76,83,90,42,49,26,33,65},
-                                                         {23,5,82,89,91,48,30,32,39,66},
-                                                         {79,6,13,95,97,29,31,38,45,72},
-                                                         {10,12,94,96,78,35,37,44,46,53},
-                                                         {11,18,100,77,84,36,43,50,27,59}
-                                                        });
-        DoubleMatrix N = new DoubleMatrix(new double[][]{{92,99,1,8,15,67,74,51,58,40},
-                                                         {98,80,7,14,16,73,55,57,64,41},
-                                                         {4,81,88,20,22,54,56,63,70,47},
-                                                         {85,87,19,21,3,60,62,69,71,28},
-                                                         {86,93,25,2,9,61,68,75,52,34},
-                                                         {17,24,76,83,90,42,49,26,33,65},
-                                                         {23,5,82,89,91,48,30,32,39,66},
-                                                         {79,6,13,95,97,29,31,38,45,72},
-                                                         {10,12,94,96,78,35,37,44,46,53},
-                                                         {11,18,100,77,84,36,43,50,27,59}
-                                                        });
-        DoubleMatrix a = new DoubleMatrix(new double[]{0.27603,0.6797,0.6551,0.16261,0.119,0.49836,0.95974,0.34039,0.58527,0.22381
-});
+        DoubleMatrix H = null;
+        DoubleMatrix g = null;
+        // Caminho para o arquivo CSV
+        String filePathH = "C:\\Users\\Isabela V. Santos\\Documents\\NetBeansProjects\\ultrassom\\arquivos teste\\M.csv";
 
-        // Verificando as dimensões de M e N para multiplicação matricial
-        if (M.columns != N.rows) {
-            System.out.println("As dimensões de M e N não são compatíveis para multiplicação matricial.");
-            return;
+        try {
+            CSVReader reader = new CSVReaderBuilder(new FileReader(filePathH))
+                .withCSVParser(new CSVParserBuilder()
+                        .withSeparator(';')
+                        .build())
+                .build();
+            
+            List<String[]> allData = reader.readAll();
+            System.out.println(allData);
+            
+            H = convertToDoubleMatrix(allData);
+            
+            // Exemplo de uso da matriz
+            System.out.println("Matriz lida do arquivo CSV:");
+            System.out.println(H);
+            
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
         }
         
-        // Verificando as dimensões de M e a para multiplicação matricial
-        if (M.columns != a.length) {
-            System.out.println("As dimensões de M e a não são compatíveis para multiplicação matricial.");
-            return;
+        
+        String filePathg = "C:\\Users\\Isabela V. Santos\\Documents\\NetBeansProjects\\ultrassom\\arquivos teste\\a.csv";
+        
+        try {
+            CSVReader reader = new CSVReaderBuilder(new FileReader(filePathg))
+                .withCSVParser(new CSVParserBuilder()
+                        .withSeparator(';')
+                        .build())
+                .build();
+            
+            List<String[]> allData = reader.readAll();
+            System.out.println(allData);
+            
+            g = convertToDoubleMatrix(allData);
+            
+            // Exemplo de uso da matriz
+            System.out.println("Vetor de sinal lido do arquivo CSV:");
+            System.out.println(g);
+            
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
         }
         
-        // Verificando as dimensões de N e a para multiplicação matricial
-        if (N.columns != a.length) {
-            System.out.println("As dimensões de N e a não são compatíveis para multiplicação matricial.");
-            return;
-        }
-
-        // Operação matricial MN = M * N
-        DoubleMatrix MN = M.mmul(N);
-        System.out.println("MN = M * N:\n" + MN);
-
-        // Operação matricial aM = a * M
-        DoubleMatrix aM = a.transpose().mmul(M);
-        System.out.println("aM = a * M:\n" + aM);
-
-        // Operação matricial Ma = M * a
-        DoubleMatrix Ma = M.mmul(a);
-        System.out.println("Ma = M * a:\n" + Ma);*/
-        
-        // Defina a matriz do sistema linear e o vetor do lado direito
-        // H - Matriz de modelo
-        // g - Vetor de sinal
-        DoubleMatrix H = new DoubleMatrix(new double[][]{{4, 1}, {1, 3}});
-        DoubleMatrix g = new DoubleMatrix(new double[]{1, 2});
 
         // Chute inicial para a solução
         DoubleMatrix f0 = DoubleMatrix.zeros(g.length);
 
+        System.out.println("g.length: ");
+        System.out.println(g.length);
         
         // Chame a função CG para resolver o sistema linear Ax = b
         DoubleMatrix solutionCGNR = CGNR(H, g, f0, 1e-10, 1000);
-        DoubleMatrix solutionCGNE = CGNE(H, g, 100);
+        //DoubleMatrix solutionCGNE = CGNE(H, g, 100);
 
         // Imprima a solução
         System.out.println("Solução CGNR encontrada: ");
         System.out.println(solutionCGNR);
-        System.out.println("Solução CGNE encontrada: ");
-        System.out.println(solutionCGNE);
+        //System.out.println("Solução CGNE encontrada: ");
+        //System.out.println(solutionCGNE);
     }
 
     private static DoubleMatrix CGNR (DoubleMatrix H, DoubleMatrix g, DoubleMatrix f0, double tolerance, int maxIterations) {
@@ -138,5 +137,24 @@ public class Ultrassom {
 
        //System.out.println(r);
         return f;
+    }
+    
+    private static DoubleMatrix convertToDoubleMatrix(List<String[]> data) {
+        int numRows = data.size();
+        int numCols = data.get(0).length;
+        
+        System.out.println("numRows: ");
+        System.out.println(numRows);
+        System.out.println("numCols: ");
+        System.out.println(numCols);
+        
+        DoubleMatrix matrix = new DoubleMatrix(numRows, numCols);
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                matrix.put(i, j, Double.parseDouble(data.get(i)[j]));
+            }
+        }
+
+        return matrix;
     }
 }
